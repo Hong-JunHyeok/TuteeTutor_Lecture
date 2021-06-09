dcc72abb65cd619b4786beb1380d8f70dd3eac4c
HongJunHyeok <edb1631@naver.com>
2021-06-09 22:38:49 +0900

    ADD array rendering

--- Lecture_1/script.js
+++ Lecture_1/script.js
@@ -1,6 +1,9 @@
 const box = document.getElementById("box");
 const box2 = document.getElementById("box2");
 const box3 = document.getElementById("box3");
+const lists = document.getElementById("lists");
+
+const array = ["학교 등교하기", "기숙사 정리하기", "웹 개발하기"];
 
 box.innerText = "JS로 넣은 텍스트입니다.";
 
@@ -11,3 +14,12 @@ box2.addEventListener("click", () => {
 //모든 CSS의 속성들을 변경할 수 있습니다.
 box3.style.borderRadius = "70%";
 box3.style.backgroundColor = "blue";
+
+//
+array.forEach((item) => {
+  const listEl = document.createElement("li");
+  listEl.classList.add("item");
+  listEl.innerText = `${item}`;
+
+  lists.appendChild(listEl);
+});
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 2021 대소고 튜티튜터 웹 강의정리
+
+**[1일차 강의내용](https://github.com/Hong-JunHyeok/TuteeTutor_Lecture/tree/main/Lecture_1)**
